c0df08cd4c4820a51ec38079994773b7220d9804
Max <maxlev823@gmail.com>
2025-08-22 17:09:37 +0300

    added hdfs dependencies to pom and docker compose, need to resolve fistun of hdfs configuration in flink

--- src/main/java/org/example/Process.java
+++ src/main/java/org/example/Process.java
@@ -2,9 +2,13 @@ package org.example;
 
 import lombok.RequiredArgsConstructor;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.serialization.SimpleStringEncoder;
 import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.core.fs.Path;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSink;
+import org.example.configuration.HdfsConfiguration;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -19,14 +23,24 @@ public class Process implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        String hdfsUri = HdfsConfiguration.hdfsConfiguration()
+                .get("fs.defaultFS", "hdfs://namenode:9000");
+
         DataStreamSource<String> dataStreamSource =
                 this.env.fromSource(this.source, this.watermarkStrategy, "kafka-source");
+
+        StreamingFileSink<String> sink = StreamingFileSink.
+                <String>forRowFormat(new Path(hdfsUri+"flink/output"), new SimpleStringEncoder<>("UTF-8"))
+                .build();
 
         dataStreamSource
                 .map(msg -> {
                     System.out.println("nice message: " + msg);
                     return msg;
                 });
+
+        dataStreamSource.addSink(sink);
+
         this.env.execute();
     }
 }
---
+++ src/main/java/org/example/configuration/HdfsConfiguration.java
@@ -0,0 +1,16 @@
+package org.example.configuration;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+
+import java.io.File;
+
+public class HdfsConfiguration {
+    public static Configuration hdfsConfiguration(){
+        Configuration configuration = new Configuration();
+        configuration.addResource(new Path(new File("./config/hdfs-site.xml").getAbsolutePath()));
+        configuration.addResource(new Path(new File("./config/core-site.xml").getAbsolutePath()));
+
+        return configuration;
+    }
+}
